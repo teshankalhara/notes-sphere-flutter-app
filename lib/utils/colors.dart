99e07926de4a248c183ac61bcf3b3c72c9b1a5f7
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-02 00:38:18 +0530

    Refactor AppColors class to use const for color properties

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,9 +3,9 @@ import 'package:flutter/material.dart';
 class AppColors {
   //primary colors
   static const Color kFabColor = Color.fromARGB(255, 204, 17, 237);
-  static Color kBgColor = const Color(0xff202326);
-  static Color kCardColor = const Color(0xff2F3235);
-  static Color kWhiteColor = const Color.fromARGB(255, 255, 255, 255);
+  static const Color kBgColor = Color(0xff202326);
+  static const Color kCardColor = Color(0xff2F3235);
+  static const Color kWhiteColor = Color.fromARGB(255, 255, 255, 255);
 
   //gradient colors
   static const int gradientStart = 0XFF01F0FF;
