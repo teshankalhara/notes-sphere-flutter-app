4a2bf672f3627c3cceaec3e30249ea340da42441
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-02 01:35:49 +0530

    Implement HomeScreen layout with AppBar and Padding

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes_sphere_flutter_app/utils/constants.dart';
+import 'package:notes_sphere_flutter_app/utils/text_styles.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +12,14 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("NotesSphere", style: AppTextStyles.appTitle),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(children: []),
+      ),
+    );
   }
 }
