efcd56566ca2d46c0090068cb0c13a9a22a2ce8b
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-02 00:38:24 +0530

    Update text styles to use AppColors for consistent color management

--- lib/utils/text_styles.dart
+++ lib/utils/text_styles.dart
@@ -1,27 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:notes_sphere_flutter_app/utils/colors.dart';
 
 class AppTextStyles {
   static const TextStyle appTitle = TextStyle(
     fontSize: 28,
-    color: Colors.white,
+    color: AppColors.kWhiteColor,
     fontWeight: FontWeight.bold,
   );
 
   static const TextStyle appSubtitle = TextStyle(
     fontSize: 24,
     fontWeight: FontWeight.w500,
-    color: Colors.white,
+    color: AppColors.kWhiteColor,
   );
 
   static const TextStyle appDescription = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w400,
-    color: Colors.white,
+    color: AppColors.kWhiteColor,
   );
 
   static const TextStyle appDescriptionSmall = TextStyle(
     fontSize: 14,
-    color: Colors.white,
+    color: AppColors.kWhiteColor,
     fontWeight: FontWeight.w400,
   );
 
@@ -29,7 +30,7 @@ class AppTextStyles {
 
   static const TextStyle appButton = TextStyle(
     fontSize: 16,
-    color: Colors.white,
+    color: AppColors.kWhiteColor,
     fontWeight: FontWeight.bold,
   );
 }
