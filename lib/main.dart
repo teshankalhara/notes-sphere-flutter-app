86291c216ef4d88df8c8876c267ac6bcfc842e8b
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-04 22:32:49 +0530

    Initialize Hive and register adapters for Note and Todo models

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:notes_sphere_flutter_app/models/note_model.dart';
+import 'package:notes_sphere_flutter_app/models/todo_model.dart';
 import 'package:notes_sphere_flutter_app/utils/router.dart';
 import 'package:notes_sphere_flutter_app/utils/theme_data.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  //init hive
+  await Hive.initFlutter();
+
+  //register adapters
+  Hive.registerAdapter(NoteAdapter());
+  Hive.registerAdapter(TodoAdapter());
+
+  //open boxes
+  await Hive.openBox('notes');
+  await Hive.openBox('todos');
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
