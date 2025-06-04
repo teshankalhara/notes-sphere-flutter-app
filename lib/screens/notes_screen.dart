3fe0db7083e3172ab0518ce071aca54e67e46d95
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-04 21:42:08 +0530

    Implement NotesScreen layout with AppBar, FloatingActionButton, and Notes title

--- lib/screens/notes_screen.dart
+++ lib/screens/notes_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:notes_sphere_flutter_app/utils/colors.dart';
+import 'package:notes_sphere_flutter_app/utils/constants.dart';
+import 'package:notes_sphere_flutter_app/utils/router.dart';
+import 'package:notes_sphere_flutter_app/utils/text_styles.dart';
 
 class NotesScreen extends StatefulWidget {
   const NotesScreen({super.key});
@@ -10,6 +14,30 @@ class NotesScreen extends StatefulWidget {
 class _NotesScreenState extends State<NotesScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("Notes")));
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            AppRouter.router.go("/"); //
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(100)),
+          side: BorderSide(color: AppColors.kWhiteColor, width: 1),
+        ),
+        child: const Icon(Icons.add, color: AppColors.kWhiteColor, size: 30),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(AppConstants.kDefaultPadding),
+        child: Column(
+          children: [const Text("Notes", style: AppTextStyles.appTitle)],
+        ),
+      ),
+    );
   }
 }
