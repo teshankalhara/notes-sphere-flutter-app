3c7f36a36bb778460ae13c715e0b1c09f817ce59
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-04 22:27:06 +0530

    Add Hive annotations to Todo model for local storage support

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,10 +1,23 @@
+import 'package:hive/hive.dart';
 import 'package:uuid/uuid.dart';
 
+part 'todo_model.g.dart';
+
+@HiveType(typeId: 2)
 class Todo {
+  @HiveField(0)
   final String id;
+
+  @HiveField(1)
   final String title;
+
+  @HiveField(2)
   final DateTime date;
+
+  @HiveField(3)
   final DateTime time;
+
+  @HiveField(4)
   final bool isDone;
 
   Todo({
