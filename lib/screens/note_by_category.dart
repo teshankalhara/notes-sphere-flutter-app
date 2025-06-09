a1245226c8250d9c63e9f63b9fb88cc684b600f0
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-09 19:03:40 +0530

    Implement grid view for displaying notes by category with NoteCategoryCard widget

--- lib/screens/note_by_category.dart
+++ lib/screens/note_by_category.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:notes_sphere_flutter_app/models/note_model.dart';
 import 'package:notes_sphere_flutter_app/services/note_service.dart';
+import 'package:notes_sphere_flutter_app/utils/constants.dart';
+import 'package:notes_sphere_flutter_app/utils/router.dart';
+import 'package:notes_sphere_flutter_app/utils/text_styles.dart';
+import 'package:notes_sphere_flutter_app/widgets/note_category_card.dart';
 
 class NoteByCategory extends StatefulWidget {
   final String category;
@@ -34,8 +38,49 @@ class _NoteByCategoryState extends State<NoteByCategory> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.category),
-        automaticallyImplyLeading: true,
+        //title: Text(widget.category),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          onPressed: () {
+            //go to the notes page
+            AppRouter.router.push("/notes");
+          },
+          icon: const Icon(Icons.arrow_back),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: AppConstants.kDefaultPadding,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: AppConstants.kDefaultHeight),
+              Text(widget.category, style: AppTextStyles.appTitle),
+              const SizedBox(height: AppConstants.kDefaultHeight),
+              GridView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: AppConstants.kDefaultPadding,
+                  mainAxisSpacing: AppConstants.kDefaultPadding,
+                  childAspectRatio: 7 / 11,
+                ),
+                itemCount: noteList.length,
+                itemBuilder: (context, index) {
+                  return NoteCategoryCard(
+                    noteContent: noteList[index].content,
+                    noteTitle: noteList[index].title,
+                    editNote: () async {},
+                    removeNote: () async {},
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
