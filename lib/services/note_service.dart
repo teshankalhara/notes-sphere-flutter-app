d73f57ece0280d2608dad9f6d7f47e6f4846db28
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-09 19:08:23 +0530

    Implement deleteNote method to remove notes by ID from the database

--- lib/services/note_service.dart
+++ lib/services/note_service.dart
@@ -91,4 +91,15 @@ class NoteService {
       print(err.toString());
     }
   }
+
+  //remove note
+  Future<void> deleteNote(String noteId) async {
+    try {
+      final dynamic allNotes = await _myBox.get("notes");
+      allNotes.removeWhere((element) => element.id == noteId);
+      await _myBox.put("notes", allNotes);
+    } catch (err) {
+      print(err.toString());
+    }
+  }
 }
