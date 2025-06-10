3038f767133ee9713bfe950351f3d407e80f31c8
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-10 19:01:09 +0530

    Add CompletedTab widget for displaying completed tasks

---
+++ lib/widgets/completed_tab.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:notes_sphere_flutter_app/utils/text_styles.dart';
+
+class CompletedTab extends StatefulWidget {
+  const CompletedTab({super.key});
+
+  @override
+  State<CompletedTab> createState() => _CompletedTabState();
+}
+
+class _CompletedTabState extends State<CompletedTab> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Completed", style: AppTextStyles.appTitle));
+  }
+}
