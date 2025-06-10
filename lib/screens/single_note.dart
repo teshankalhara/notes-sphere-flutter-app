c1dc0947c23a573132819ed527b652d1c299d362
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-10 17:47:44 +0530

    Refactor SingleNoteScreen to format and display note date, title, and content with improved styling

--- lib/screens/single_note.dart
+++ lib/screens/single_note.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:notes_sphere_flutter_app/models/note_model.dart';
+import 'package:notes_sphere_flutter_app/utils/colors.dart';
+import 'package:notes_sphere_flutter_app/utils/constants.dart';
+import 'package:notes_sphere_flutter_app/utils/text_styles.dart';
 
 class SingleNoteScreen extends StatefulWidget {
   final Note note;
@@ -13,6 +17,34 @@ class SingleNoteScreen extends StatefulWidget {
 class _SingleNoteScreenState extends State<SingleNoteScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Single Note")));
+    final formattedDate = DateFormat.yMMMd().format(widget.note.date);
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Note")),
+      body: Padding(
+        padding: const EdgeInsets.all(AppConstants.kDefaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 20),
+            Text(widget.note.title, style: AppTextStyles.appTitle),
+            const SizedBox(height: 5),
+            Text(
+              formattedDate,
+              style: AppTextStyles.appDescriptionSmall.copyWith(
+                color: AppColors.kFabColor,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              widget.note.content,
+              style: AppTextStyles.appDescription.copyWith(
+                color: AppColors.kWhiteColor.withOpacity(0.3),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
