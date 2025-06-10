07ea033170d00864338c47cf50f82ebbfdd8e93e
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-10 19:01:04 +0530

    Implement tabbed navigation for To-Do and Completed tasks in TodoScreen

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:notes_sphere_flutter_app/utils/colors.dart';
+import 'package:notes_sphere_flutter_app/utils/router.dart';
+import 'package:notes_sphere_flutter_app/utils/text_styles.dart';
+import 'package:notes_sphere_flutter_app/widgets/completed_tab.dart';
+import 'package:notes_sphere_flutter_app/widgets/todo_tab.dart';
 
 class TodoScreen extends StatefulWidget {
   const TodoScreen({super.key});
@@ -7,9 +12,47 @@ class TodoScreen extends StatefulWidget {
   State<TodoScreen> createState() => _TodoScreenState();
 }
 
-class _TodoScreenState extends State<TodoScreen> {
+class _TodoScreenState extends State<TodoScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("To-Do List")));
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            AppRouter.router.go("/");
+          },
+        ),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(child: Text("To-Do", style: AppTextStyles.appDescription)),
+            Tab(child: Text("Completed", style: AppTextStyles.appDescription)),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(100)),
+          side: BorderSide(color: AppColors.kWhiteColor, width: 1),
+        ),
+        child: const Icon(Icons.add, color: AppColors.kWhiteColor, size: 30),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [TodoTab(), CompletedTab()],
+      ),
+    );
   }
 }
