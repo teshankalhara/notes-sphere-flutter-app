5eec30987a7b788d07222826a22240ba36078bd9
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-15 16:04:24 +0530

    Refactor TodoScreen to improve todo management and UI interaction

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_sphere_flutter_app/helpers/snack_bar.dart';
 import 'package:notes_sphere_flutter_app/models/todo_model.dart';
 import 'package:notes_sphere_flutter_app/services/todo_service.dart';
 import 'package:notes_sphere_flutter_app/utils/colors.dart';
@@ -20,8 +21,17 @@ class _TodoScreenState extends State<TodoScreen>
   TodoService todoService = TodoService();
 
   late List<Todo> allTodos = [];
-  late List<Todo> incompletedTodos = [];
+  late List<Todo> inCompletedTodos = [];
   late List<Todo> completedTodos = [];
+
+  final TextEditingController _todoController = TextEditingController();
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _todoController.dispose();
+    super.dispose();
+  }
 
   @override
   void initState() {
@@ -43,9 +53,97 @@ class _TodoScreenState extends State<TodoScreen>
     final List<Todo> loadedTodos = await todoService.loadTodos();
     setState(() {
       allTodos = loadedTodos;
-      incompletedTodos = allTodos.where((t) => !t.isDone).toList();
+      inCompletedTodos = allTodos.where((t) => !t.isDone).toList();
       completedTodos = allTodos.where((t) => t.isDone).toList();
     });
+  }
+
+  void _addTodo() async {
+    try {
+      if (_todoController.text.isNotEmpty) {
+        final Todo newToDo = Todo(
+          title: _todoController.text,
+          date: DateTime.now(),
+          time: DateTime.now(),
+          isDone: false,
+        );
+
+        await todoService.addTodo(newToDo);
+
+        setState(() {
+          allTodos.add(newToDo);
+          inCompletedTodos.add(newToDo);
+        });
+
+        AppHelpers.showSnackBar(context, "Task added successfully!");
+        Navigator.pop(context);
+      }
+    } catch (e) {
+      print(e.toString());
+      AppHelpers.showSnackBar(context, "Task added Failed!");
+    }
+  }
+
+  void openMessageModal(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: AppColors.kCardColor,
+          title: Text(
+            "Add Task",
+            style: AppTextStyles.appDescription.copyWith(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: AppColors.kWhiteColor,
+            ),
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                TextField(
+                  controller: _todoController,
+                  style: TextStyle(color: AppColors.kWhiteColor, fontSize: 20),
+                  decoration: InputDecoration(
+                    hintText: "Enter your task",
+                    hintStyle: AppTextStyles.appDescriptionSmall,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                _addTodo();
+                AppRouter.router.go("/todos");
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(AppColors.kFabColor),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+              ),
+              child: const Text("Add Task", style: AppTextStyles.appButton),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text(
+                'Cancel',
+                style: TextStyle(color: AppColors.kWhiteColor),
+              ),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -68,7 +166,9 @@ class _TodoScreenState extends State<TodoScreen>
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          openMessageModal(context);
+        },
         shape: RoundedRectangleBorder(
           borderRadius: const BorderRadius.all(Radius.circular(100)),
           side: BorderSide(color: AppColors.kWhiteColor, width: 1),
@@ -78,8 +178,11 @@ class _TodoScreenState extends State<TodoScreen>
       body: TabBarView(
         controller: _tabController,
         children: [
-          TodoTab(incompletedTodos: allTodos),
-          CompletedTab(completedTodos: completedTodos),
+          TodoTab(inCompletedTodos: allTodos, completedTodos: completedTodos),
+          CompletedTab(
+            completedTodos: completedTodos,
+            inCompletedTodos: inCompletedTodos,
+          ),
         ],
       ),
     );
