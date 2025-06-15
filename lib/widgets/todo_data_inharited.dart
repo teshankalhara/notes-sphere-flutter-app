f5db53f1a67f2d67b2e00cd49c34b51b041d3a5c
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-15 17:55:24 +0530

    Rename ToDoData to TodoData for consistency and clarity

--- lib/widgets/todo_data_inharited.dart
+++ lib/widgets/todo_data_inharited.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:notes_sphere_flutter_app/models/todo_model.dart';
 
-class ToDoData extends InheritedWidget {
+class TodoData extends InheritedWidget {
   final List<Todo> todos;
   final Function() onTodosChanged;
 
-  const ToDoData({
+  const TodoData({
     super.key,
     required this.todos,
     required this.onTodosChanged,
     required super.child,
   });
 
-  static ToDoData? of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<ToDoData>();
+  static TodoData? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<TodoData>();
   }
 
   @override
-  bool updateShouldNotify(covariant ToDoData oldWidget) {
+  bool updateShouldNotify(covariant TodoData oldWidget) {
     //print('updateShouldNotify');
     return todos != oldWidget.todos;
   }
