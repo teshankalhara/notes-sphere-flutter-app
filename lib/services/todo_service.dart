bb32b31f46e780e4606130852ba8378fab702aea
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-15 16:04:35 +0530

    Refactor TodoService to standardize key names and enhance todo management methods

--- lib/services/todo_service.dart
+++ lib/services/todo_service.dart
@@ -34,16 +34,69 @@ class TodoService {
   //if new user init todos
   Future<void> createInitialTodos() async {
     if (_myBox.isEmpty) {
-      await _myBox.put("TODOS", todos);
+      await _myBox.put("todos", todos);
     }
   }
 
   //load todos
   Future<List<Todo>> loadTodos() async {
-    final dynamic todoList = _myBox.get("TODOS");
+    final dynamic todoList = _myBox.get("todos");
     if (todoList != null && todoList is List<dynamic>) {
       return todoList.cast<Todo>().toList();
     }
     return [];
+  }
+
+  //marks todo as done
+  Future<void> markAsDone(Todo todo) async {
+    try {
+      final dynamic allTodos = await _myBox.get("todos");
+      final int index = allTodos.indexWhere((element) => element.id == todo.id);
+
+      allTodos[index] = todo;
+
+      await _myBox.put("todos", allTodos);
+    } catch (err) {
+      print(err.toString());
+    }
+  }
+
+  // Method to add a todo
+  Future<void> addTodo(Todo todo) async {
+    try {
+      //get all todos from the box
+      final dynamic allTodos = await _myBox.get("todos");
+      allTodos.add(todo);
+      await _myBox.put("todos", allTodos);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  //update todo
+  Future<void> updateTodo(Todo todo) async {
+    try {
+      //get all todos from the box
+      final dynamic allTodos = await _myBox.get("todos");
+      final int index = allTodos.indexWhere((element) => element == todo);
+
+      allTodos[index] = todo;
+      await _myBox.put("todos", allTodos);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  //delete todo
+  Future<void> deleteTodo(Todo todo) async {
+    try {
+      //get all todos from the box
+      final dynamic allTodos = await _myBox.get("todos");
+
+      allTodos.remove(todo);
+      await _myBox.put("todos", allTodos);
+    } catch (e) {
+      print(e);
+    }
   }
 }
