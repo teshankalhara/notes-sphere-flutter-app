a13d015099f13e76e646565e3f34d5f2e351f2c0
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-15 13:51:06 +0530

    Refactor TodoTab to accept incompletedTodos and display them in a ListView

--- lib/widgets/todo_tab.dart
+++ lib/widgets/todo_tab.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:notes_sphere_flutter_app/models/todo_model.dart';
+import 'package:notes_sphere_flutter_app/utils/constants.dart';
 import 'package:notes_sphere_flutter_app/utils/text_styles.dart';
 
 class TodoTab extends StatefulWidget {
-  const TodoTab({super.key});
+  final List<Todo> incompletedTodos;
+
+  const TodoTab({super.key, required this.incompletedTodos});
 
   @override
   State<TodoTab> createState() => _TodoTabState();
@@ -11,6 +15,21 @@ class TodoTab extends StatefulWidget {
 class _TodoTabState extends State<TodoTab> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("To-Do", style: AppTextStyles.appTitle));
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: AppConstants.kDefaultPadding),
+      child: Column(
+        children: [
+          const SizedBox(height: AppConstants.kDefaultHeight * 2),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.incompletedTodos.length,
+              itemBuilder: (context, index) {
+                final todo = widget.incompletedTodos[index];
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
