5f17d3b73e458433bb6412c49ad210cb1e043ee9
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-15 16:04:29 +0530

    Refactor CompletedTab to handle marking todos as done and update state accordingly

--- lib/widgets/completed_tab.dart
+++ lib/widgets/completed_tab.dart
@@ -1,20 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:notes_sphere_flutter_app/helpers/snack_bar.dart';
 import 'package:notes_sphere_flutter_app/models/todo_model.dart';
+import 'package:notes_sphere_flutter_app/services/todo_service.dart';
 import 'package:notes_sphere_flutter_app/utils/constants.dart';
+import 'package:notes_sphere_flutter_app/utils/router.dart';
 import 'package:notes_sphere_flutter_app/widgets/todo_card.dart';
 
 class CompletedTab extends StatefulWidget {
   final List<Todo> completedTodos;
+  final List<Todo> inCompletedTodos;
 
-  const CompletedTab({super.key, required this.completedTodos});
+  const CompletedTab({
+    super.key,
+    required this.completedTodos,
+    required this.inCompletedTodos,
+  });
 
   @override
   State<CompletedTab> createState() => _CompletedTabState();
 }
 
 class _CompletedTabState extends State<CompletedTab> {
+  void _marksTodoAsDone(Todo todo) async {
+    try {
+      final Todo updatedTodo = Todo(
+        id: todo.id,
+        title: todo.title,
+        date: todo.date,
+        time: todo.time,
+        isDone: true,
+      );
+
+      await TodoService().markAsDone(updatedTodo);
+
+      setState(() {
+        widget.completedTodos.remove(todo);
+        widget.inCompletedTodos.add(updatedTodo);
+      });
+
+      AppHelpers.showSnackBar(context, "Todo marked as undone");
+
+      AppRouter.router.go("/todos");
+    } catch (err) {
+      print(err.toString());
+      AppHelpers.showSnackBar(context, "Failed to marked as undone");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      widget.completedTodos.sort((a, b) => a.time.compareTo(b.time));
+    });
+
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: AppConstants.kDefaultPadding),
       child: Column(
@@ -28,7 +66,9 @@ class _CompletedTabState extends State<CompletedTab> {
                 return TodoCard(
                   toDo: todo,
                   isComplete: true,
-                  onCheckBoxChanged: () {},
+                  onCheckBoxChanged: () {
+                    _marksTodoAsDone(todo);
+                  },
                 );
               },
             ),
