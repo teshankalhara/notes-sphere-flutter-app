e3b4de6bd1c3714c316baaf539641dca9a09c35a
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-15 16:04:41 +0530

    Refactor TodoTab to standardize variable names and enhance todo completion logic

--- lib/widgets/todo_tab.dart
+++ lib/widgets/todo_tab.dart
@@ -1,20 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:notes_sphere_flutter_app/helpers/snack_bar.dart';
 import 'package:notes_sphere_flutter_app/models/todo_model.dart';
+import 'package:notes_sphere_flutter_app/services/todo_service.dart';
 import 'package:notes_sphere_flutter_app/utils/constants.dart';
+import 'package:notes_sphere_flutter_app/utils/router.dart';
 import 'package:notes_sphere_flutter_app/widgets/todo_card.dart';
 
 class TodoTab extends StatefulWidget {
-  final List<Todo> incompletedTodos;
+  final List<Todo> inCompletedTodos;
+  final List<Todo> completedTodos;
 
-  const TodoTab({super.key, required this.incompletedTodos});
+  const TodoTab({
+    super.key,
+    required this.inCompletedTodos,
+    required this.completedTodos,
+  });
 
   @override
   State<TodoTab> createState() => _TodoTabState();
 }
 
 class _TodoTabState extends State<TodoTab> {
+  void _marksTodoAsDone(Todo todo) async {
+    try {
+      final Todo updatedTodo = Todo(
+        id: todo.id,
+        title: todo.title,
+        date: todo.date,
+        time: todo.time,
+        isDone: true,
+      );
+      await TodoService().markAsDone(updatedTodo);
+
+      setState(() {
+        widget.inCompletedTodos.remove(todo);
+        widget.completedTodos.add(updatedTodo);
+      });
+
+      AppHelpers.showSnackBar(context, "Todo marked as done");
+
+      AppRouter.router.go("/todos");
+    } catch (err) {
+      print(err.toString());
+      AppHelpers.showSnackBar(context, "Failed to marked as done");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    setState(() {
+      widget.inCompletedTodos.sort((a, b) => a.time.compareTo(b.time));
+    });
+
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: AppConstants.kDefaultPadding),
       child: Column(
@@ -22,13 +59,15 @@ class _TodoTabState extends State<TodoTab> {
           const SizedBox(height: AppConstants.kDefaultHeight * 2),
           Expanded(
             child: ListView.builder(
-              itemCount: widget.incompletedTodos.length,
+              itemCount: widget.inCompletedTodos.length,
               itemBuilder: (context, index) {
-                final todo = widget.incompletedTodos[index];
+                final todo = widget.inCompletedTodos[index];
                 return TodoCard(
                   toDo: todo,
                   isComplete: false,
-                  onCheckBoxChanged: () {},
+                  onCheckBoxChanged: () {
+                    _marksTodoAsDone(todo);
+                  },
                 );
               },
             ),
