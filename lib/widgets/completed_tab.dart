18ee7525362bd96aeb66adefbb1749f0772485aa
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-06-15 15:15:28 +0530

    Refactor CompletedTab to display completed todos in a ListView

--- lib/widgets/completed_tab.dart
+++ lib/widgets/completed_tab.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_sphere_flutter_app/models/todo_model.dart';
-import 'package:notes_sphere_flutter_app/utils/text_styles.dart';
+import 'package:notes_sphere_flutter_app/utils/constants.dart';
+import 'package:notes_sphere_flutter_app/widgets/todo_card.dart';
 
 class CompletedTab extends StatefulWidget {
   final List<Todo> completedTodos;
@@ -14,6 +15,26 @@ class CompletedTab extends StatefulWidget {
 class _CompletedTabState extends State<CompletedTab> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Completed", style: AppTextStyles.appTitle));
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: AppConstants.kDefaultPadding),
+      child: Column(
+        children: [
+          const SizedBox(height: AppConstants.kDefaultHeight * 2),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.completedTodos.length,
+              itemBuilder: (context, index) {
+                final todo = widget.completedTodos[index];
+                return TodoCard(
+                  toDo: todo,
+                  isComplete: true,
+                  onCheckBoxChanged: () {},
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
